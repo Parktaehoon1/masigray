0002a205ebe2dccb0baf5d61dd77d3f208953c90
Parktaehoon1 <92parkth@gmail.com>
2022-09-27 14:20:19 +0900

    팝업창 수정

--- js/script.js
+++ js/script.js
@@ -1,199 +1,211 @@
 // html, css, js 로딩완료
 $(document).ready(function () {
-	// 모달창
-	let modalWrap = $('.modal-wrap');
-	let modalClose = $('.modal-close');
+  // 모달창
+  let modalWrap = $(".modal-wrap");
+  let modalClose = $(".modal-close");
 
+  modalClose.click(function () {
+    modalWrap.stop().fadeOut(500);
+    // 추가기능 : 스크롤바 살리기
+    // $('html').css('overflow', 'auto');
+  });
+  let modalMain = $(".modal-main");
+  //내용 배경 클릭
+  modalMain.click(function (event) {
+    // 클릭 정보 전달 막기
+    event.stopPropagation();
+  });
+  //전체 배경 클릭
+  modalWrap.click(function () {
+    modalWrap.stop().fadeOut(500);
+    // 추가기능 : 스크롤바 살리기
+    // $('html').css('overflow', 'auto');
+  });
+  $("html").keydown(function (key) {
+    if (key.keyCode == 13) {
+      modalWrap.stop().fadeOut(200);
+      $("html").css("overflow", "auto");
+    }
+  });
 
-	modalClose.click(function () {
-		modalWrap.stop().fadeOut(500)
-		// 추가기능 : 스크롤바 살리기
-		// $('html').css('overflow', 'auto');
-	});
-	let modalMain = $('.modal-main')
-	//내용 배경 클릭
-	modalMain.click(function (event) {
-		// 클릭 정보 전달 막기
-		event.stopPropagation();
-	});
-	//전체 배경 클릭
-	modalWrap.click(function () {
-		modalWrap.stop().fadeOut(500);
-		// 추가기능 : 스크롤바 살리기
-		// $('html').css('overflow', 'auto');
-	});
+  // 메인 메뉴 기능
+  let header = $(".header");
+  let header_top = $(".header-top");
 
-	// 메인 메뉴 기능
-	let header = $('.header');
-	let header_top = $('.header-top');
+  let header_main = $(".header-main");
+  let gnb = $(".gnb");
+  gnb.mouseenter(function () {
+    header_main.addClass("header-main-show");
+  });
+  gnb.mouseleave(function () {
+    header_main.removeClass("header-main-show");
+  });
 
-	let header_main = $('.header-main');
-	let gnb = $('.gnb');
-	gnb.mouseenter(function () {
-		header_main.addClass('header-main-show');
-	});
-	gnb.mouseleave(function () {
-		header_main.removeClass('header-main-show');
-	});
+  // 스크롤시 기능
+  $(window).scroll(function () {
+    // 스크롤바의 위치를 파악한다.
+    let temp = $(window).scrollTop();
+    // .header-top 숨기기
+    if (temp > 0) {
+      header.addClass("header-fix");
+      header_top.addClass("header-top-close");
+    } else {
+      header.removeClass("header-fix");
+      header_top.removeClass("header-top-close");
+    }
+  });
+  // 모바일 메뉴 기능
+  // 1. 펼침메뉴 기능
+  let mb_menu_li = $(".mb-menu > li");
+  let mb_mainmenu = $(".mb-menu > li > a");
+  let mb_submenu = $(".mb-submenu");
 
-	// 스크롤시 기능
-	$(window).scroll(function () {
-		// 스크롤바의 위치를 파악한다.
-		let temp = $(window).scrollTop();
-		// .header-top 숨기기
-		if (temp > 0) {
-			header.addClass('header-fix');
-			header_top.addClass('header-top-close');
-		} else {
-			header.removeClass('header-fix');
-			header_top.removeClass('header-top-close');
-		}
+  $.each(mb_mainmenu, function (index) {
+    // 인덱스가 들어가면 mb_main_menu의 0,1,2,순서들어감
+    $(this).click(function (event) {
+      // href를 막는다.
+      event.preventDefault();
 
-	});
-	// 모바일 메뉴 기능
-	// 1. 펼침메뉴 기능
-	let mb_menu_li = $('.mb-menu > li');
-	let mb_mainmenu = $('.mb-menu > li > a');
-	let mb_submenu = $('.mb-submenu');
+      // 클릭하면 현재 포커스 클래스가 있는지 검토
+      let temp = $(this).hasClass("mb-menu-focus");
 
-	$.each(mb_mainmenu, function (index) {
-		// 인덱스가 들어가면 mb_main_menu의 0,1,2,순서들어감
-		$(this).click(function (event) {
-			// href를 막는다.
-			event.preventDefault();
+      if (temp) {
+        // 포커스 색상 적용해제
+        $(this).removeClass("mb-menu-focus");
+        // 아이콘 모션 해제
+        $(this).removeClass("mb-icon-rot");
+        // 펼처진 해당 서브 메뉴를 닫아준다.
+        mb_submenu.eq(index).hide();
+      } else {
+        // 일단 모두 숨겨라
+        mb_submenu.hide();
 
-			// 클릭하면 현재 포커스 클래스가 있는지 검토
-			let temp = $(this).hasClass('mb-menu-focus');
+        // 일단 모든 포커스 색상을 해제한다.
+        mb_mainmenu.removeClass("mb-menu-focus");
 
-			if (temp) {
+        // 일단 아이콘을 원래대로 돌려라
+        mb_mainmenu.removeClass("mb-icon-rot");
+        // 포커스 색상 적용하기
+        $(this).addClass("mb-menu-focus");
+        // 아이콘을 돌리자.
+        $(this).addClass("mb-icon-rot");
+        // 클릭된 번호만 보여라
+        mb_submenu.eq(index).show();
+      }
+    });
+  });
+  // 모바일 메뉴 보이기 기능
+  let mb_dim = $(".mb-dim"); // 가림막
+  let mb_bt = $(".mb-bt"); // 햄버거
+  mb_bt.click(function (event) {
+    // a 태그의 href 를 막는다
+    event.preventDefault();
+    // 배경 보여주기
+    mb_dim.show(); // show가 display block 효과
+    // 메뉴 보여지는 과정
+    mb_wrap.addClass("mb-wrap-open");
+  });
 
-				// 포커스 색상 적용해제
-				$(this).removeClass('mb-menu-focus');
-				// 아이콘 모션 해제 
-				$(this).removeClass('mb-icon-rot')
-				// 펼처진 해당 서브 메뉴를 닫아준다.
-				mb_submenu.eq(index).hide();
+  mb_dim.click(function () {
+    // 배경을 숨긴다.
+    mb_dim.hide();
+    // 주메뉴 색상 제거
+    mb_mainmenu.removeClass("mb-menu-focus");
+    // 서브메뉴 모두 닫기
+    mb_submenu.hide();
+    // 사라지는 모션실행
+    mb_wrap.removeClass("mb-wrap-open");
+  });
 
-			} else {
-				// 일단 모두 숨겨라 
-				mb_submenu.hide();
-
-				// 일단 모든 포커스 색상을 해제한다.
-				mb_mainmenu.removeClass('mb-menu-focus');
-
-				// 일단 아이콘을 원래대로 돌려라 
-				mb_mainmenu.removeClass('mb-icon-rot')
-				// 포커스 색상 적용하기
-				$(this).addClass('mb-menu-focus');
-				// 아이콘을 돌리자.
-				$(this).addClass('mb-icon-rot')
-				// 클릭된 번호만 보여라
-				mb_submenu.eq(index).show();
-
-			}
-		});
-	});
-	// 모바일 메뉴 보이기 기능
-	let mb_dim = $('.mb-dim'); // 가림막
-	let mb_bt = $('.mb-bt') // 햄버거
-	mb_bt.click(function (event) {
-		// a 태그의 href 를 막는다
-		event.preventDefault();
-		// 배경 보여주기
-		mb_dim.show(); // show가 display block 효과
-		// 메뉴 보여지는 과정
-		mb_wrap.addClass('mb-wrap-open');
-	})
-
-	// 모바일 메뉴 숨기기 기능
-	let mb_close = $('.mb-close')
-	let mb_wrap = $('.mb-wrap');
-	mb_close.click(function (event) {
-		// a 태그의 href 막아준다.
-		event.preventDefault();
-		// 배경을 숨긴다.
-		mb_dim.hide();
-		// 주메뉴 색상 제거
-		mb_mainmenu.removeClass('mb-menu-focus');
-		// 서브메뉴 모두 닫기
-		mb_submenu.hide();
-		// 사라지는 모션실행
-		mb_wrap.removeClass('mb-wrap-open');
-	})
-	// 반응형 처리
-	$(window).resize(function () {
-		let temp = $(window).width();
-		if (temp > 760) {
-			mb_dim.hide();
-			mb_wrap.removeClass('mb-wrap-open');
-			mb_mainmenu.removeClass('mb-menu-focus');
-			mb_mainmenu.removeClass('mb-icon-rot');
-			mb_submenu.hide();
-
-		}
-	});
-	// 위로가기
-	let go_top = $('.gotop');
-	go_top.click(function () {
-		$('html').animate({
-			scrollTop: 0
-		}, 1000);
-	});
-
+  // 모바일 메뉴 숨기기 기능
+  let mb_close = $(".mb-close");
+  let mb_wrap = $(".mb-wrap");
+  mb_close.click(function (event) {
+    // a 태그의 href 막아준다.
+    event.preventDefault();
+    // 배경을 숨긴다.
+    mb_dim.hide();
+    // 주메뉴 색상 제거
+    mb_mainmenu.removeClass("mb-menu-focus");
+    // 서브메뉴 모두 닫기
+    mb_submenu.hide();
+    // 사라지는 모션실행
+    mb_wrap.removeClass("mb-wrap-open");
+  });
+  // 반응형 처리
+  $(window).resize(function () {
+    let temp = $(window).width();
+    if (temp > 760) {
+      mb_dim.hide();
+      mb_wrap.removeClass("mb-wrap-open");
+      mb_mainmenu.removeClass("mb-menu-focus");
+      mb_mainmenu.removeClass("mb-icon-rot");
+      mb_submenu.hide();
+    }
+  });
+  // 위로가기
+  let go_top = $(".gotop");
+  go_top.click(function () {
+    $("html").animate(
+      {
+        scrollTop: 0,
+      },
+      1000
+    );
+  });
 });
 
 // html, css, js, 멀티미디어까지 로딩완료
 window.onload = function () {
-	// visual 슬라이드
-	new Swiper('.sw-visual', {
-		loop: true,
-		autoplay: {
-			delay: 1000,
-			disableOnInteraction: false,
-		},
-		speed: 1000,
-		navigation: {
-			prevEl: '.sw-visual-prev',
-			nextEl: '.sw-visual-next'
-		}
-	});
+  // visual 슬라이드
+  new Swiper(".sw-visual", {
+    loop: true,
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction: false,
+    },
+    speed: 1000,
+    navigation: {
+      prevEl: ".sw-visual-prev",
+      nextEl: ".sw-visual-next",
+    },
+  });
 
-	// items 슬라이드
-	new Swiper('.sw-items', {
-		loop: true,
-		speed: 800,
-		autoplay: {
-			delay: 1000,
-			disableOnInteraction: false,
-		},
+  // items 슬라이드
+  new Swiper(".sw-items", {
+    loop: true,
+    speed: 800,
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction: false,
+    },
 
-		navigation: {
-			prevEl: '.sw-items-prev',
-			nextEl: '.sw-items-next'
-		},
+    navigation: {
+      prevEl: ".sw-items-prev",
+      nextEl: ".sw-items-next",
+    },
 
-		pagination: {
-			el: '.sw-items-pg'
-		}
+    pagination: {
+      el: ".sw-items-pg",
+    },
+  });
 
-	});
+  // Bevarage 슬라이드
+  new Swiper(".sw-bevarage", {
+    loop: true,
+    speed: 1000,
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      prevEl: ".sw-bevarage-prev",
+      nextEl: ".sw-bevarage-next",
+    },
 
-	// Bevarage 슬라이드
-	new Swiper('.sw-bevarage', {
-		loop: true,
-		speed: 1000,
-		autoplay: {
-			delay: 1000,
-			disableOnInteraction: false,
-		},
-		navigation: {
-			prevEl: '.sw-bevarage-prev',
-			nextEl: '.sw-bevarage-next'
-		},
-
-		pagination: {
-			el: '.sw-bevarage-pg',
-			type: 'fraction'
-		}
-	});
-}
+    pagination: {
+      el: ".sw-bevarage-pg",
+      type: "fraction",
+    },
+  });
+};
